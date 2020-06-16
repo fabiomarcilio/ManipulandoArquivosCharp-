c58a0e1cadb060e16602ddc4c2080a2bf53cd77b
Fabio Marcilio <fabiomarcilio@msn.com>
2020-06-15 22:24:16 -0300

    exemplo de trabalho com arquivos C#

---
+++ TrabalhandoComArquivos/Entities/Products.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabalhandoComArquivos.Entities
+{
+    class Products
+    {
+        public string Name { set; get; }
+        public double Price { set; get; }
+
+        public int Quantity { set; get; }
+
+        public Products(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+            return Price * Quantity;
+        }
+
+    }
+}
